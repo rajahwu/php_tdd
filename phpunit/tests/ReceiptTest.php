e2d4bc2ce46c758b0b6d6c97e97eea736795bd7c
rajahwu <vmradford@gmail.com>
2023-09-07 22:10:08 -0400

    create mock test object

--- phpunit/tests/ReceiptTest.php
+++ phpunit/tests/ReceiptTest.php
@@ -13,17 +13,38 @@ class ReceiptTest extends TestCase {
     // public function tearDown() {
     //     unset($this->Receipt);
     // }
-
-    public function testTotal() {
+/**
+ * @dataProvider provideTotal
+ */
+    public function testTotal($items, $expected) {
         $Receipt = new Receipt();
-        $input = [0,2,5,8];
-        $output = $Receipt->total($input);
+        $coupon = null;
+        $output = $Receipt->total($items, $coupon);
         $this->assertEquals(
-            15,
+            $expected,
             $output,
-            'When summing the total should equal 15'
+            "When summing the total should equal {$expected}"
         );
     }
+    public function provideTotal() {
+        return [
+            [[1,2,5,8], 16],
+            [[-1,2,5,8], 14],
+            [[1,2,8], 11],
+        ];
+    }
+    public function testTotalAndCoupon() {
+        $Receipt = new Receipt();
+        $input = [0,2,5,8];
+        $coupon = 0.20;
+        $output = $Receipt->total($input, $coupon);
+        $this->assertEquals(
+            12,
+            $output,
+            'When summing the total should equal 12'
+        );
+    }
+    
     
     public function testTax() {
         $Receipt = new Receipt();
@@ -36,4 +57,23 @@ class ReceiptTest extends TestCase {
             "The tax calculation should equal 1.00"
         );
     }
+    
+    // public function testPostTaxTotal() {
+    //     $items = [1,2,5,8];
+    //     $tax = 0.20;
+    //     $coupon = null;
+    //     $Receipt = $this->getMockBuilder('TDD\Receipt')
+    //         ->setMethods(['tax', 'total'])
+    //         ->getMock();
+    //     $Receipt->expects($this->once())
+    //         ->method('total')
+    //         ->with($items, $coupon)
+    //         ->will($this->returnValue(10.00));
+    //         $Receipt->expects($this->once())
+    //         ->method('tax')
+    //         ->with(10.00, $tax)
+    //         ->will($this->returnValue(1.00));
+    //     $result = $Receipt->postTaxTotal([1,2,3,8], 0.20, null);
+    //     $this->assertEquals(11.00, $result);
+    // }
 }
